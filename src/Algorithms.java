a67401d40c1aa9bc8d8c69f6d94cacddcd9794f7
CarlosMCS97 <carlosmiguel.castro@students.salle.url.edu>
2024-12-31 17:35:39 +0100

    Added the time calculation formula

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,5 +1,44 @@
 public class Algorithms {
 
+    public float timeCalculation(Intern intern, Task task) {
+        float time = 0;
+        float avg = intern.getAverage();
+        float skillPoints = 0;
+        float taskTime = task.getTime();
+
+        if (avg >= 5 && avg < 7) {
+            skillPoints = avg * 10;
+        }
+        else {
+            if (avg >= 7 && avg < 9) {
+                skillPoints = (float) (avg * 10 * 1.2);
+            }
+            else {
+                if (avg >= 9) {
+                    skillPoints = (float) (avg * 10 * 1.5);
+                }
+            }
+        }
+
+        if (intern.isJunior()) {
+            skillPoints += 10;
+        }
+
+        if (intern.getSubject().equals(task.getSubject())) {
+            skillPoints += 20;
+        }
+
+        //Ensure skillPoints is greater than 0 to avoid division by zero
+        if (skillPoints == 0) {
+            throw new IllegalArgumentException("Skill points is 0.");
+        }
+
+        time = (taskTime / skillPoints) * 80;
+
+        return time;
+    }
+
+
     public void bruteForce() {
 
     }
