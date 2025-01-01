d3a5822abf80ecb00951892d973770027f458334
CarlosMCS97 <carlosmiguel.castro@students.salle.url.edu>
2025-01-01 16:45:00 +0100

    Updating the BNB algorith

--- src/Algorithms.java
+++ src/Algorithms.java
@@ -1,3 +1,6 @@
+import java.util.List;
+import java.util.PriorityQueue;
+
 public class Algorithms {
 
     public float timeCalculation(Intern intern, Task task) {
@@ -48,8 +51,48 @@ public class Algorithms {
     }
 
     public void branchAndBound(Task[] tasks, Intern[] interns, int numTasks) {
+        PriorityQueue<BNBConfig> queue = new PriorityQueue<>();
 
+        BNBConfig first = new BNBConfig(numTasks);
+        queue.offer(first);
+        float minCost = Float.MAX_VALUE;
+        BNBConfig bestSolution = null;
 
+        while (!queue.isEmpty()) {
+            BNBConfig current = queue.poll();
+
+            if (current.internIndex < interns.length) {
+                List<BNBConfig> children = current.expand(tasks,interns,this);
+
+                for (BNBConfig child : children) {
+                    //updating the bound
+                    child.bound = child.estimatedCost(tasks, interns, this);
+
+                    if (child.bound < minCost) {
+                        queue.offer(child);
+
+                        if (allTasksAssigned(child.assigned) && child.currentCost < minCost) {
+                            minCost = child.currentCost;
+                            bestSolution = child;
+                        }
+
+                    }
+                }
+
+            }
+
+        }
+
+        System.out.println("Minimum Time: " + minCost);
+    }
+
+    private boolean allTasksAssigned(boolean[] assigned) {
+        for (boolean task : assigned) {
+            if (!task) {
+                return false;
+            }
+        }
+        return true;
     }
 
     public void greedyTO() {
---
+++ src/BNBConfig.java
@@ -0,0 +1,80 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class BNBConfig implements Comparable<BNBConfig> {
+
+    int internIndex;
+    boolean[] assigned;
+    float currentCost;
+    float bound;
+    int difficulty;
+    String building;
+
+    public BNBConfig(int numTask) {
+        this.internIndex = 0;
+        this.assigned = new boolean[numTask];
+        this.currentCost = 0;
+        this.bound = 0;
+        this.difficulty = 0;
+        this.building = null;
+    }
+
+    public BNBConfig(BNBConfig that) {
+        this.internIndex = that.internIndex;
+        this.assigned = that.assigned.clone();
+        this.currentCost = that.currentCost;
+        this.bound = that.bound;
+        this.difficulty = that.difficulty;
+        this.building = that.building;
+    }
+
+    @Override
+    public int compareTo(BNBConfig that) {
+        return Float.compare(this.bound, that.bound);
+    }
+
+    public List<BNBConfig> expand(Task[] tasks, Intern[] interns, Algorithms algorithm) {
+        List<BNBConfig> children = new ArrayList<>();
+
+        for (int i = 0; i < tasks.length; i++) {
+            if (!assigned[i]) {
+                BNBConfig child = new BNBConfig(this);
+                Intern intern = interns[internIndex];
+                Task task = tasks[i];
+
+                if (intern.isJunior() && (child.difficulty + task.getDifficulty()) > 40) {
+                    continue;
+                }
+
+                if (child.building != null && !child.building.equals(task.getBuilding())) {
+                    continue;
+                }
+
+                child.building = task.getBuilding();
+
+                float time = algorithm.timeCalculation(intern, task);
+                child.currentCost += time;
+                child.assigned[i] = true;
+                child.difficulty += intern.isJunior() ? task.getDifficulty() : 0;
+                child.internIndex++;
+
+                children.add(child);
+
+            }
+        }
+
+        return children;
+    }
+
+    public float estimatedCost(Task[] tasks, Intern[] interns, Algorithms algorithm) {
+        float estimate = currentCost;
+        for (int i = internIndex; i < tasks.length; i++) {
+            if (!assigned[i]) {
+                Intern intern = interns[internIndex];
+                Task task = tasks[i];
+                estimate += algorithm.timeCalculation(intern, task);
+            }
+        }
+        return estimate;
+    }
+}
